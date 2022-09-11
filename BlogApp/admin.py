e47bf1efbb63b67ed471e6524824715ccdeffd94
Franco Villarreal <franconicolasvillarreal@gmail.com>
2022-09-11 16:15:39 -0300

    Add models to Admin Panel

--- BlogApp/admin.py
+++ BlogApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from BlogApp.models import Post, PostComment, User
+
+admin.site.register(Post)
+admin.site.register(PostComment)
+admin.site.register(User)
